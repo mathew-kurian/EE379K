34c5532ccc33de9c2386fdbebb25b56999ab9149
Kapil Gowru <kgowru@kapils-mbp.kadirnet>
2014-09-21 23:44:16 -0500

    small changes to commit

--- Homework 2/q3/src/q3/BakeryLock.java
+++ Homework 2/q3/src/q3/BakeryLock.java
@@ -31,7 +31,6 @@ public class BakeryLock implements MyLock {
 			if( k != myId){
 				while(flag[k]){};
 				while((label[k] != 0) && ((label[k]) < label[myId]) || ((label[k] == label[myId]) && k < myId)) {};
-
 			};
 		};
 	}
