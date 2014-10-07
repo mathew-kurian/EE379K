81719fa83492fad0db60cda4c0f5eee3abb91c08
mwkurian <mwkurian@ubuntu.(none)>
2014-10-06 21:35:20 -0700

    matrix read fix

--- Homework 3/q4/MonteCarloPi/monte_carlo_pi.cpp
+++ Homework 3/q4/MonteCarloPi/monte_carlo_pi.cpp
@@ -7,46 +7,46 @@
 #include <time.h>
 
 int inline imax(int x, int y){
-	return x > y ? x : y;
+  return x > y ? x : y;
 }
 
 double MonteCarloPi(int s)
 {
-	int valid = 0, samples = 0, threads = 0;
-	double x, y;
+  int valid = 0, samples = 0, threads = 0;
+  double x, y;
 
-    #pragma omp parallel private(x, y,samples, threads) reduction(+:valid) 
-	{
-		threads = omp_get_num_threads();
-		samples = imax(s / threads, 1);
+  #pragma omp parallel private(x, y,samples, threads) reduction(+:valid) 
+  {
+    threads = omp_get_num_threads();
+    samples = imax(s / threads, 1);
 
-		srand(int(time(NULL)) ^ threads);
-		
-		#pragma omp for
-		for (int i = 0; i < samples; ++i)
-		{
-			x = ((double)rand()) / ((double)RAND_MAX);
-			y = ((double)rand()) / ((double)RAND_MAX);
+    srand(int(time(NULL)) ^ threads);
+    
+    #pragma omp for
+    for (int i = 0; i < samples; ++i)
+    {
+      x = ((double)rand()) / ((double)RAND_MAX);
+      y = ((double)rand()) / ((double)RAND_MAX);
 
-			if ((x * x + y * y) <= 1.0){
-				++valid;
-			}
-		}
-	}
+      if ((x * x + y * y) <= 1.0){
+        ++valid;
+      }
+    }
+  }
 
-	return (((double)valid) / ((double)s)) * 4.0;
+  return (((double)valid) / ((double)s)) * 4.0;
 }
 
 int main(int argc, char* argv[])
 {
-	using namespace std;
+  using namespace std;
 
-	if(argc < 2){
-		cout << "Error: Arguments length < 1" << endl;
-		return EXIT_FAILURE;
-	}
+  if(argc < 2){
+    cout << "Error: Arguments length < 1" << endl;
+    return EXIT_FAILURE;
+  }
 
-	cout << MonteCarloPi(atoi(argv[1])) << endl;
+  cout << MonteCarloPi(atoi(argv[1])) << endl;
 
-	return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
