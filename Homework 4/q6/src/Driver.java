ea29c69d72d33c0610104b15b97a0a457549277e
Mathew Kurian <bluejamesbond@gmail.com>
2014-10-28 00:12:35 -0500

    Fixing Kapil's mistakes...ahhahaha

--- Homework 4/q6/src/Driver.java
+++ Homework 4/q6/src/Driver.java
@@ -1,9 +1,9 @@
-import q6.CLHLock;
+import q6.MCSLock;
 
 public class Driver {
 
 	public static void main(String[] args) {
-		Test test = new Test(new CLHLock(), 8, 1000);
+		Test test = new Test(new MCSLock(), 2, 1000);
 		test.start();
 	}
 
