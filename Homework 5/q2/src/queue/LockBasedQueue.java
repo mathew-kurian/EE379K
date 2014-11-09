a9ef1ee08f7bf4b6a5780a548f2f515bc6782db8
kgowru <k.gowru@gmail.com>
2014-11-09 12:50:25 -0600

    added lock based unbounded queue not sure if its right '

--- Homework 5/q2/src/queue/LockBasedQueue.java
+++ Homework 5/q2/src/queue/LockBasedQueue.java
@@ -1,6 +1,7 @@
 package queue;
 
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.atomic.AtomicReferenceArray;
 
 //single consumer lock based unbounded queue 
@@ -11,6 +12,7 @@ public class LockBasedQueue<T> extends Queue<T> {
 	final AtomicLong tail;
 	Node<T> headNode;
 	final AtomicReference<Node<T>> tailNode;
+	final int mask;
 	
 	//constructor
 	//parameter is the initial estimated capacity for the Queue
@@ -21,22 +23,60 @@ public class LockBasedQueue<T> extends Queue<T> {
 		array = new AtomicReferenceArray<T>(capacity);
 		mask = array.length() -1;
 		head = new AtomicLong();
-		
-		
+		tail = new AtomicLong();
+		headNode = new Node<T>(null);
+		tailNode = new AtomicReference<Node<T>>(headNode);
 		
 	}
 	//similar to offer
 	@Override
 	public void enqueue(T t) {
-		// TODO Auto-generated method stub
-
+		if (t == null){
+			throw new NullPointerException();
+		}
+		
+		long t1 = tail.getAndIncrement();
+		long h = head.get();
+		if ((t1-h) < array.length()){
+			int index = (int) (t1 & mask);
+			array.lazySet(index, t);
+		} else {
+			Node<T> node = new Node<T> (t);
+			tailNode.getAndSet(node).lazySet(node);
+		}
 	}
 
 	//similar to poll
 	@Override
 	public T dequeue() {
-		// TODO Auto-generated method stub
-		return null;
+		long h = head.get();
+		long t = tail.get();
+		if (h == t){
+			return null;
+		}
+		
+		int index = (int) (h & mask);
+		T t1 = array.get(index);
+		if (t1 == null){
+			Node<T> next = headNode.get();
+			headNode = next;
+			t1 = next.value;
+		} else {
+			array.lazySet(index, null);
+		}
+		
+		head.lazySet(h + 1);
+		return t1;
 	}
+	
+	static final class Node<T> extends AtomicReference<Node<T>> {
+	    private static final long serialVersionUID = 1L;
+
+	    final T value;
+
+	    Node(T value) {
+	      this.value = value;
+	    }
+	  }
 
 }
