780a0a967bade6ddf449b19a7c0a6fe689038b20
Mathew Kurian <bluejamesbond@gmail.com>
2014-11-09 19:59:48 -0600

    Fixed timeout

--- Homework 5/q2/src/SequentialCollectionTests.java
+++ Homework 5/q2/src/SequentialCollectionTests.java
@@ -1,12 +1,17 @@
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.Timeout;
 
 public class SequentialCollectionTests {
 
 	private static final String OPERATIONS = "12000";
 
-	@Test(timeout=5000)
+    @Rule
+    public Timeout TIMEOUT = new Timeout(4000);
+    
+	@Test
 	public void lockFreeQueueTest() {
 		for (int i = 1; i <= 6; i++) {
 			Driver.main(new String[] { "queue", "lock-free", Integer.toString(i), OPERATIONS });
@@ -15,7 +20,7 @@ public class SequentialCollectionTests {
 		assertTrue(true);
 	}
 
-	@Test(timeout=5000)
+	@Test
 	public void lockBasedQueueTest() {
 		for (int i = 1; i <= 6; i++) {
 			Driver.main(new String[] { "queue", "lock-based", Integer.toString(i), OPERATIONS });
@@ -24,7 +29,7 @@ public class SequentialCollectionTests {
 		assertTrue(true);
 	}
 
-	@Test(timeout=5000)
+	@Test
 	public void lockFreeStackTest() {
 		for (int i = 1; i <= 6; i++) {
 			Driver.main(new String[] { "stack", "lock-free", Integer.toString(i), OPERATIONS });
@@ -33,7 +38,7 @@ public class SequentialCollectionTests {
 		assertTrue(true);
 	}
 
-	@Test(timeout=5000)
+	@Test
 	public void lockFreeContentionManagedStackTest() {
 		for (int i = 1; i <= 6; i++) {
 			Driver.main(new String[] { "stack", "lock-free-contention-managed", Integer.toString(i), OPERATIONS });
@@ -42,7 +47,7 @@ public class SequentialCollectionTests {
 		assertTrue(true);
 	}
 
-	@Test(timeout=5000)
+	@Test
 	public void lockBasedStackTest() {
 		for (int i = 1; i <= 6; i++) {
 			Driver.main(new String[] { "stack", "lock-based", Integer.toString(i), OPERATIONS });
