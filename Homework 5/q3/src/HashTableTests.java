5d06f4dbe3a86ccaa107a067c96345909e457c8c
Mathew Kurian <bluejamesbond@gmail.com>
2014-11-09 20:00:27 -0600

    Added timeout

--- Homework 5/q3/src/HashTableTests.java
+++ Homework 5/q3/src/HashTableTests.java
@@ -2,6 +2,8 @@ import static org.junit.Assert.*;
 
 public class HashTableTests {
 
+	private static final String OPERATIONS = "12000";
+	
 	@org.junit.Test
 	public void lockFree() {
 		Driver.main(new String[] { "lock-free", "6", "12000" });
