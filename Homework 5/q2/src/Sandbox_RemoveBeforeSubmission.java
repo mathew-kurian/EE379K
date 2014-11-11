b4911791f26d731df7fe9f2a6db4f6babfe02522
Mathew Kurian <bluejamesbond@gmail.com>
2014-11-10 21:47:06 -0600

    Fixing LockBasedQueue. I think. Doublecheck for me

--- Homework 5/q2/src/Sandbox_RemoveBeforeSubmission.java
+++ Homework 5/q2/src/Sandbox_RemoveBeforeSubmission.java
@@ -2,25 +2,46 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import queue.LockBasedQueue;
 import stack.LockBasedStack;
 
 public class Sandbox_RemoveBeforeSubmission {
 
-//	@Test
-//	public void lockBasedStackSimpleTest() {
-//		LockBasedStack<Integer> lbs = new LockBasedStack<Integer>();
-//		lbs.push(10);
-//		lbs.push(9);
-//		lbs.push(8);
-//		lbs.push(7);
-//
-//		assertTrue(lbs.pop().equals(7));
-//		assertTrue(lbs.pop().equals(8));
-//		assertTrue(lbs.pop().equals(9));
-//		assertTrue(lbs.pop().equals(10));
-//		assertTrue(lbs.pop() == null);
-//		assertTrue(true);
-//	}
+	@Test
+	public void lockBasedStackSimpleTest() {
+		LockBasedStack<Integer> lbs = new LockBasedStack<Integer>();
+
+		assertTrue(lbs.pop() == null);
+		
+		lbs.push(10);
+		lbs.push(9);
+		lbs.push(8);
+		lbs.push(7);
+
+		assertTrue(lbs.pop().equals(7));
+		assertTrue(lbs.pop().equals(8));
+		assertTrue(lbs.pop().equals(9));
+		assertTrue(lbs.pop().equals(10));
+		assertTrue(lbs.pop() == null);
+	}
+	
+	@Test
+	public void lockBasedQueueSimpleTest() {
+		LockBasedQueue<Integer> lbs = new LockBasedQueue<Integer>();
+
+		assertTrue(lbs.dequeue() == null);
+		
+		lbs.enqueue(10);
+		lbs.enqueue(9);
+		lbs.enqueue(8);
+		lbs.enqueue(7);
+
+		assertTrue(lbs.dequeue().equals(10));
+		assertTrue(lbs.dequeue().equals(9));
+		assertTrue(lbs.dequeue().equals(8));
+		assertTrue(lbs.dequeue().equals(7));
+		assertTrue(lbs.dequeue() == null);
+	}
 	
 	@Test
 	public void myTest(){
--- Homework 5/q2/src/queue/LockFreeQueue.java
+++ Homework 5/q2/src/queue/LockFreeQueue.java
@@ -4,60 +4,62 @@ import java.util.concurrent.atomic.AtomicReference;
 
 //single consumer lock free unbounded queue
 public class LockFreeQueue<T> extends Queue<T> {
-	
+
 	private static class Node<T> {
 		final T item;
 		final AtomicReference<Node<T>> next;
-		public Node(T item, Node<T> next){
+
+		public Node(T item, Node<T> next) {
 			this.item = item;
-			this.next = new AtomicReference<Node<T>> (next);
+			this.next = new AtomicReference<Node<T>>(next);
 		}
 	}
-	
+
 	private final Node<T> temp;
 	private final AtomicReference<Node<T>> head;
 	private final AtomicReference<Node<T>> tail;
 
-	public LockFreeQueue(){
-		temp = new Node<T>(null,null);
+	public LockFreeQueue() {
+		temp = new Node<T>(null, null);
 		head = new AtomicReference<Node<T>>(temp);
 		tail = new AtomicReference<Node<T>>(temp);
 	}
-	
-	//puts t into the queue, returns true when successful
+
+	// puts t into the queue, returns true when successful
 	@Override
 	public boolean enqueue(T t) {
-		Node<T> newNode = new Node<T> (t,null);
-		while(true){
+		Node<T> newNode = new Node<T>(t, null);
+		while (true) {
 			Node<T> currTail = tail.get();
 			Node<T> nextTail = currTail.next.get();
-			if(currTail == tail.get()){
-				if(nextTail!=null){
+			if (currTail == tail.get()) {
+				if (nextTail != null) {
 					tail.compareAndSet(currTail, nextTail);
 				} else {
-					if(currTail.next.compareAndSet(null, newNode)){
+					if (currTail.next.compareAndSet(null, newNode)) {
 						tail.compareAndSet(currTail, newNode);
 						return true;
 					}
 				}
 			}
-		
+
 		}
 	}
 
-	//takes T out of the queue
+	// takes T out of the queue
 	@Override
 	public T dequeue() {
-		for(;;){
+		while (true) {
 			Node<T> oldHead = head.get();
 			Node<T> oldTail = tail.get();
 			Node<T> oldNextHead = oldHead.next.get();
-			if(oldHead == head.get()){
-				if(oldHead == oldTail){
-					if(oldNextHead == null) return null;
+			if (oldHead == head.get()) {
+				if (oldHead == oldTail) {
+					if (oldNextHead == null)
+						return null;
 					tail.compareAndSet(oldTail, oldNextHead);
 				} else {
-					if(head.compareAndSet(oldTail, oldNextHead))
+					if (head.compareAndSet(oldTail, oldNextHead))
 						return oldNextHead.item;
 				}
 			}
