c50e7e6c16098fc03664a0a83120e3c61775fab9
Mathew Kurian <bluejamesbond@gmail.com>
2014-11-18 02:55:58 -0600

    Adding tester for optimal concurrent search size

--- Parallel Convex Hull/src/com/computation/Main.java
+++ Parallel Convex Hull/src/com/computation/Main.java
@@ -3,6 +3,7 @@ package com.computation;
 import com.computation.algo.*;
 import com.computation.common.ConvexHull;
 import com.computation.common.Point2DCloud;
+import com.computation.experimental.OptimalSearch;
 
 import javax.swing.*;
 import java.lang.reflect.InvocationTargetException;
@@ -16,12 +17,15 @@ public class Main {
 
         Point2DCloud.DPI_SCALING = 2; /* Set display scaling */
 
+        OptimalSearch optimalSearch = new OptimalSearch();
+        optimalSearch.find();
+
         //Mathew's Concurrent Quick Hull Implementation
 //        ConvexHull convexHull = new QuickHull(/* pointCount */ 100, /* width */ 800, /* height */ 600, /* threadCount */ 10);
 //        convexHull.show();
 
         //Kapil's Concurrent Gift Wrapping Implemntation (max at 4 threads)
-        ConvexHull convexHull = new GiftWrapping(100, 1200, 1200, 15, true);
-        convexHull.show();
+//        ConvexHull convexHull = new GiftWrapping(100, 1200, 1200, 15, true);
+//        convexHull.show();
     }
 }
---
+++ Parallel Convex Hull/src/com/computation/experimental/OptimalSearch.java
@@ -0,0 +1,96 @@
+package com.computation.experimental;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by mwkurian on 11/18/2014.
+ */
+public class OptimalSearch {
+
+    static final int MIN_SIZE = 20000;
+    static final int MAX_SIZE = 15000000;
+    static final int OFFSET_SIZE = 1000000;
+    static final int MIN_THREADS = 10;
+    static final int MAX_THREADS = 20;
+    static final int OFFSET_THREADS = 1;
+
+    private static List<Integer> generate(int length) {
+        List<Integer> data = new ArrayList<Integer>();
+        Random rand = new Random();
+
+        for (int i = 0; i < length; i++) {
+            data.add(rand.nextInt());
+        }
+
+        return data;
+    }
+
+    public void find() {
+        findHelper(MIN_THREADS, MIN_SIZE, new ArrayList<Result>());
+    }
+
+    private void findHelper(final int threads, final int length, final ArrayList<Result> results) {
+
+        if (threads >= MAX_THREADS) {
+            System.out.println("herere");
+            // Done
+            Collections.sort(results);
+            for (Result r : results) {
+                System.out.printf("(size: %d)(threads: %d)(time: %fs)\n",
+                        r.size, r.threads, r.toString());
+            }
+            return;
+        }
+
+        if (length >= MAX_SIZE) {
+            findHelper(threads + OFFSET_THREADS, MIN_SIZE, results);
+            return;
+        }
+
+        final List<Integer> searchData = OptimalSearch.generate(length);
+
+        final ConcurrentSearch cs =
+                new ConcurrentSearch<Integer>(searchData, Integer.MIN_VALUE, threads) {
+                    @Override
+                    public void found(Integer integer) {
+                        super.found(integer);
+                        results.add(new Result(length, threads, elapsed()));
+                        findHelper(threads, length + OFFSET_SIZE, results);
+                    }
+                };
+
+        if(threads == MIN_THREADS) {
+            new SimpleSearch<Integer>(searchData, Integer.MIN_VALUE) {
+                @Override
+                public void found(Integer integer) {
+                    super.found(integer);
+                    results.add(new Result(length, threads, elapsed()));
+                    cs.start();
+                }
+            }.start();
+        } else {
+            cs.start();
+        }
+    }
+
+    private class Result implements Comparable<Result> {
+        public final int size;
+        public final int threads;
+        public final double time;
+
+        public Result(int size, int threads, double time) {
+            this.size = size;
+            this.threads = threads;
+            this.time = time;
+        }
+
+        @Override
+        public int compareTo(Result o) {
+            return Double.compare(time, o.time);
+        }
+    }
+
+}
