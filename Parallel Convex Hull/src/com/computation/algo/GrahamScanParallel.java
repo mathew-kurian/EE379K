15c27aeb231e6bbd91a3dd91893c1cc1c80785d3
Mathew Kurian <bluejamesbond@gmail.com>
2014-11-20 22:13:13 -0600

    Updating graham

--- Parallel Convex Hull/src/com/computation/algo/GrahamScanParallel.java
+++ Parallel Convex Hull/src/com/computation/algo/GrahamScanParallel.java
@@ -55,7 +55,7 @@ public class GrahamScanParallel extends ConvexHull {
                     if (dx1 >= 0 && dx2 < 0) {
                         return -1;
                     } else if (dx2 >= 0 && dx1 < 0) {
-                        return +1;
+                        return 1;
                     } else {
                         return 0;
                     }
